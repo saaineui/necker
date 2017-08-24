f8c63b60b65c2645c2401d750303f3ddd9fbbb2d
saaineui <saaineui@gmail.com>
2017-08-24 00:04:55 -0700

    Add cells association to Column (forgot to save)

--- app/models/column.rb
+++ app/models/column.rb
@@ -1,4 +1,5 @@
 class Column < ApplicationRecord
   belongs_to :datasheet
+  has_many :cells
   validates :name, presence: true
 end
--- spec/models/column_spec.rb
+++ spec/models/column_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe Column, type: :model do
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to belong_to(:datasheet) }
+  it { is_expected.to have_many(:cells) }
 end
