4b5cfbee9954b0e37251302b7e798b8f6aa8d18f
saaineui <saaineui@gmail.com>
2017-09-09 17:48:43 -0700

    Separate 2016 and 2017 words charts

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -2,20 +2,30 @@ class WordsController < ApplicationController
   def index
     columns = %i[new_york_times wall_street_journal washington_post]
     options = {
-      bar_width: 80,
+      bar_width: 55,
       single_y_scale: true
     }
-
-    @alt_right = {
-      collection: Word.where(word: 'alt-right').order(:start_date), 
-      columns: columns,
-      options: options.merge(title: '"alt-right" on News Homepages')
+    
+    ranges = {
+      '2016' => Date.new(2016, 1, 1)..Date.new(2016, 12, 31),
+      '2017' => Date.new(2017, 1, 1)..Date.new(2017, 12, 31)
     }
     
-    @identity = {
-      collection: Word.where(word: 'identity politics').order(:start_date), 
-      columns: columns,
-      options: options.merge(title: '"identity politics" on News Homepages')
-    }
+    @alt_right = {}
+    @identity = {}
+    
+    ranges.each do |year, range|
+      @alt_right[year] = {
+        collection: Word.where(word: 'alt-right', start_date: range).order(:start_date), 
+        columns: columns,
+        options: options.merge(title: '"alt-right" on News Homepages (' + year + ')')
+      }
+
+      @identity[year] = {
+        collection: Word.where(word: 'identity politics', start_date: range).order(:start_date), 
+        columns: columns,
+        options: options.merge(title: '"identity politics" on News Homepages (' + year + ')')
+      }
+    end
   end
 end
