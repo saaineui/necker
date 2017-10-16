7ac40a32e2da3716276e6ee9b46c459a21543e76
saaineui <saaineui@gmail.com>
2017-10-16 10:17:04 -0700

    Expand words charts to 3 rows each

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -8,24 +8,25 @@ class WordsController < ApplicationController
     }
     
     ranges = {
-      '2016' => Date.new(2016, 1, 1)..Date.new(2016, 12, 31),
-      '2017' => Date.new(2017, 1, 1)..Date.new(2017, 12, 31)
+      'January 2016 - July 2016' => Date.new(2016, 1, 1)..Date.new(2016, 7, 31),
+      'August 2016 - February 2017' => Date.new(2016, 8, 1)..Date.new(2016, 2, 29),
+      'March 2017 - September 2017' => Date.new(2017, 3, 1)..Date.new(2017, 9, 30)
     }
     
-    @alt_right = {}
-    @identity = {}
+    @alt_right = []
+    @identity = []
     
-    ranges.each do |year, range|
-      @alt_right[year] = {
+    ranges.each do |name, range|
+      @alt_right << {
         collection: Word.where(word: 'alt-right', start_date: range).order(:start_date), 
         columns: columns,
-        options: options.merge(title: '"alt-right" on News Homepages (' + year + ')')
+        options: options.merge(title: '"alt-right" on News Homepages (' + name + ')')
       }
 
-      @identity[year] = {
+      @identity << {
         collection: Word.where(word: 'identity politics', start_date: range).order(:start_date), 
         columns: columns,
-        options: options.merge(title: '"identity politics" on News Homepages (' + year + ')')
+        options: options.merge(title: '"identity politics" on News Homepages (' + name + ')')
       }
     end
   end
