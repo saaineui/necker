0b55675cb91860154ef42ce65bca071c5302d650
saaineui <saaineui@gmail.com>
2017-11-02 01:44:13 -0700

    Filter by word

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -5,16 +5,12 @@ class WordsController < ApplicationController
       width: 1000, data_formatters: %i[date delimiter]
     }
     
-    @alt_right = {
-      collection: Word.all.order(:start_date), 
-      columns: columns,
-      options: options.merge(title: '"alt-right" on News Homepages')
-    }
-
-    @identity = {
-      collection: Word.all.order(:start_date), 
-      columns: columns,
-      options: options.merge(title: '"identity politics" on News Homepages')
-    }
+    @args = ['alt-right', 'identity politics'].map do |word|
+      {
+        collection: Word.where(word: word).order(:start_date), 
+        columns: columns,
+        options: options.merge(title: "#{word} on News Homepages")
+      }
+    end
   end
 end
